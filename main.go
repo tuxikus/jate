0286537dc86d34f3b2fa54b1bfbd71039c3bbefa
tuxikus <contact@tuxikus.de>
2025-07-10 23:02:54 +0200

    added appendBuffer and simple tilde drawing like vim by obtaining the terminal width and heigth

--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/term" // used to enable raw mode, maybe change to syscalls directly
+	"golang.org/x/term" // used to enable raw mode or get the terminal size, maybe change to syscalls directly
 	"io"
 	"os"
 )
 
 // struct to hold global editor stuff
 type Editor struct {
+	Rows int
+	Columns int
 	OldTermState *term.State // used to restore the editor config after enabling raw mode
 }
 
+// used to call write only once per refresh
+type AppendBuffer struct {
+	chars []byte
+}
+
 var editor = Editor{
+	Rows: 0,
+	Columns: 0,
 	OldTermState: nil,
+}
+
+func appendBufferAppend(ab *AppendBuffer, chars []byte) {
+	ab.chars = append(ab.chars, chars...) // ... is unpacking
+}
+
+func normalExit() {
+	os.Stdin.Write([]byte("\x1b[2J")) // clear
+	os.Stdin.Write([]byte("\x1b[H")) // move cursor to 1 1
+	disableRawMode()
+	fmt.Println(editor.Rows)
+	os.Exit(0)
 }
 
 func enableRawMode() {
@@ -28,22 +49,74 @@ func disableRawMode() {
 	term.Restore(int(os.Stdin.Fd()), editor.OldTermState)
 }
 
+func readKey() byte {
+	buf := make([]byte, 1)
+	_, err := os.Stdin.Read(buf)
+	if err == io.EOF {
+		normalExit()
+	}
+	return buf[0]
+}
+
+func drawRows(ab *AppendBuffer) {
+	for i := range editor.Columns {
+		appendBufferAppend(ab, []byte("~"))
+		if i < editor.Columns - 1 {
+			appendBufferAppend(ab, []byte("\r\n"))
+		}
+	}
+}
+
+func refreshScreen() {
+	var appendBuffer AppendBuffer
+
+	// clear the screen
+	appendBufferAppend(&appendBuffer, []byte("\x1b[2J"))
+	// reposition the cursor to the beginning
+	// H: VT100 cursor position
+	// [10;10H move cursor to row 10 and column 10
+	// default is 1;1
+	appendBufferAppend(&appendBuffer, []byte("\x1b[H"))
+
+	drawRows(&appendBuffer)
+
+	appendBufferAppend(&appendBuffer, []byte("\x1b[H"))
+
+	os.Stdin.Write(appendBuffer.chars) // the only write call per refresh
+}
+
+func processKey() {
+	c := readKey()
+
+	switch c {
+	// C-q
+	case 17:
+		normalExit()
+	default:
+		fmt.Println(c)
+	}
+}
+
+// get the dimensions of the used terminal
+func getTerminalSize() {
+	columns, rows, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Println(err)
+	}
+	editor.Columns = columns
+	editor.Rows = rows
+}
+
+func initialize() {
+	getTerminalSize()
+}
+
 func main() {
 	enableRawMode()
-	buf := make([]byte, 1)
-	for {
-		_, err := os.Stdin.Read(buf)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			break
-		}
-		if buf[0] == 113 {
-			break
-		}
+	initialize()
 
-		fmt.Println(buf[0])
+	for {
+		refreshScreen()
+		processKey()
 	}
-	disableRawMode()
 }
