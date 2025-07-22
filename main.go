9d54a8941d1c63bdef44e188cdf9e3f18c34e12f
tuxikus <contact@tuxikus.de>
2025-07-22 12:34:29 +0200

    added forward and backward search

--- main.go
+++ main.go
@@ -650,17 +650,46 @@ func save() {
 	editor.fileModified = 0
 }
 
+var lastMatch = -1
+var searchDirection = 1
+
 func searchCallback(query []byte, key int) {
 	if key == '\r' || key == '\x1b' {
+		lastMatch = -1
+		searchDirection = 1
 		return
+	} else if key == KEY_ARROW_RIGHT || key == KEY_ARROW_DOWN {
+		searchDirection = 1
+	} else if key == KEY_ARROW_RIGHT || key == KEY_ARROW_UP {
+		searchDirection = -1
+	} else {
+		lastMatch = -1
+		searchDirection = 1
 	}
 
-	for i, row := range editor.row {
+	if lastMatch == -1 {
+		searchDirection = 1
+	}
+
+	current := lastMatch
+
+	for range editor.row {
+		current += searchDirection
+
+		if current == -1 {
+			current = editor.rows - 1
+		} else if current == editor.rows {
+			current = 0
+		}
+
+		row := &editor.row[current]
+
 		s := string(row.chars)
 		match := strings.Index(s, string(query))
 		if match != -1 {
-			editor.cursorY = i
-			editor.cursorX = renderXtoCursorX(&row, match)
+			lastMatch = current
+			editor.cursorY = current
+			editor.cursorX = renderXtoCursorX(row, match)
 			editor.rowOffset = editor.rows
 			break
 		}
@@ -668,7 +697,17 @@ func searchCallback(query []byte, key int) {
 }
 
 func search() {
-	prompt("Search: ", searchCallback)
+	oldCursorX := editor.cursorX
+	oldCursorY := editor.cursorY
+	oldColumnOffset := editor.columnOffset
+	oldRowOffset := editor.rowOffset
+
+	if prompt("Search: ", searchCallback) == nil {
+		editor.cursorX = oldCursorX
+		editor.cursorY = oldCursorY
+		editor.columnOffset = oldColumnOffset
+		editor.rowOffset = oldRowOffset
+	}
 }
 
 func prompt(prompt string, promptCallback PromptCallback) []byte {
